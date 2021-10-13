d3f8ff393e7f2cea3f060325af82e5331668c426
Giovanni Almeida <giovanni.almeida@mercadolivre.com>
2021-10-13 17:43:21 -0300

    feat: api blueprint and search endpoint

--- meliarena/__init__.py
+++ meliarena/__init__.py
@@ -5,7 +5,9 @@ def create_app(config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     from meliarena.home import bp as home_bp
+    from meliarena.api import bp as api_bp
 
     app.register_blueprint(home_bp, url_prefix="/")
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
