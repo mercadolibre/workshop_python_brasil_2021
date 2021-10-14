e87ed4b76ae2d35472d2fa205f082196eab4a27f
Giovanni Almeida <giovanni.almeida@mercadolivre.com>
2021-10-14 15:59:44 -0300

    feat: qa endpoint

--- meliarena/api/items.py
+++ meliarena/api/items.py
@@ -8,9 +8,11 @@ BASE_API_URL = "https://api.mercadolibre.com"
 ITEMS_SEARCH_API_URL = f"{BASE_API_URL}/sites/MLB/search"
 ITEMS_API_URL = f"{BASE_API_URL}/items"
 ITEMS_REVIEWS_API_URL = f"{BASE_API_URL}/reviews/item"
+ITEMS_QUESTIONS_API_URL = f"{BASE_API_URL}/questions/search"
 
 
 bp = Blueprint("api", __name__)
+
 
 @bp.route("/items/search/")
 def items_search():
@@ -20,6 +22,7 @@ def items_search():
     results = response.json().get("results", [])
 
     return jsonify(results)
+
 
 @bp.route("/items/compare/")
 def items_compare():
@@ -35,16 +38,19 @@ def items_compare():
     if is_comparable(items):
         items = filter_comparable_attributes(items)
     else:
-        abort(Response("Non comparable items!", status=400, mimetype='application/json'))
+        abort(
+            Response("Non comparable items!", status=400, mimetype="application/json")
+        )
 
     return jsonify(items)
+
 
 @bp.route("/items/", methods=["POST"])
 def items():
     data = request.json
 
     if data.get("id", None) is None:
-        abort(Response("Item id is required!", status=400, mimetype='application/json'))
+        abort(Response("Item id is required!", status=400, mimetype="application/json"))
 
     item = Item.query.filter_by(id=data.get("id")).first()
 
@@ -57,6 +63,7 @@ def items():
     item.save()
 
     return jsonify(item.to_dict())
+
 
 @bp.route("/items/<item_id>/reviews", methods=["GET"])
 def api_items_reviews(item_id):
@@ -73,5 +80,20 @@ def api_items_reviews(item_id):
     if result.get("reviews"):
         result["rating_average"] = response.json().get("rating_average")
         result["rating_levels"] = response.json().get("rating_levels")
+
+    return jsonify(result)
+
+
+@bp.route("/items/<item_id>/questions", methods=["GET"])
+def api_items_questions(item_id):
+    params = {"item": item_id}
+
+    response = requests.get(f"{ITEMS_QUESTIONS_API_URL}", params)
+
+    result = [
+        {"question": q.get("text"), "answer": q.get("answer").get("text")}
+        for q in response.json().get("questions", [])
+        if (q.get("status").lower() == "answered")
+    ]
 
     return jsonify(result)
