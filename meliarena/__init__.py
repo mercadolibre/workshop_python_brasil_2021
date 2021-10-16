570463ab88962a8512877b6eb8a54062889f8b35
Giovanni Almeida <giovanni.almeida@mercadolivre.com>
2021-10-16 12:04:12 -0300

    chore: moving the import to the beginning of the module

--- meliarena/__init__.py
+++ meliarena/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from meliarena.views import Home
 
 
 def create_app(config=None):
     app = Flask(__name__, instance_relative_config=True)
-
-    from meliarena.views import Home
 
     app.add_url_rule("/", view_func=Home.as_view("home"))
 
